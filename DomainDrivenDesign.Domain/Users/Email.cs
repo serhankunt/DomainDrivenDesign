b04dc8feb07e696c9998eeba08adca654518d7b5
serhankunt <hserhan006@gmail.com>
2024-05-23 13:03:48 +0300

    User klasörü parçalara ayrıldı.

---
+++ DomainDrivenDesign.Domain/Users/Email.cs
@@ -0,0 +1,20 @@
+﻿namespace DomainDrivenDesign.Domain.Users;
+
+public sealed record Email
+{
+    public string Value { get; init; }
+    public Email(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new ArgumentNullException("You cannot enter empty email address");
+        }
+
+        if(!value.Contains("@"))
+        {
+            throw new ArgumentException("Please enter valid email address");
+        }
+
+        Value = value;
+    }
+}
